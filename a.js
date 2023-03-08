1bfa8232bbfb41626c1ed7d8a3b6aa4726d6287e
Nguyễn Duy Khánh <khanh@MacBook-Air-cua-Nguyen.local>
2023-03-08 16:02:10 +0700

    add .

---
+++ a.js
@@ -0,0 +1,10 @@
+
+inputWidth = prompt('Nhập chiều dài');
+inputHeight = prompt('Nhập chiều cao');
+
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+
+document.write("Diên tích là: " + area);
